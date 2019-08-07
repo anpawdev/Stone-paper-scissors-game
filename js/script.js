2fedef4db7bf8b1adb275c2ae8f11ba79ab4f003
Ania Pawlik <pawlikanna24@gmail.com>
2019-08-07 14:07:02 +0200

    Fix playerMove

--- js/script.js
+++ js/script.js
@@ -1,77 +1,70 @@
 'use strict';
 
-var player;
-var computer;
-var game;
-var computerMove;
-var playerMove;
-// var computerResult = 0;
-// var playerResult = 0;
-// var gamesNum = 0;
-// var gamesMax = 0;
-var paper = document.getElementById('paper-button');
-var stone = document.getElementById('stone-button');
-var scissors = document.getElementById('scissors-button');
+// var player;
+// var computer;
+// var game;
+// var computerMove;
 var output = document.getElementById('output');
 var result = document.getElementById('result');
 var newGame = document.getElementById('new-game');
-var end = document.getElementById('end');
 var params = {
   computerResult: 0,
   playerResult: 0,
   gamesNum: 0,
   gamesMax: 0,
+  progress: []
+}
+var move = {
+  1: 'paper',
+  2: 'stone',
+  3: 'scissors'
 }
 
 
 //funkcja ruch gracza  
-var playerMove = function (move) {
-  if (move == 'paper') {
-    player = 'paper';
-  } else if (move == 'stone') {
-    player = 'stone';
-  } else if (move == 'scissors') {
-    player = 'scissors';
-  }
+var player = document.querySelectorAll('.player-move');
+
+for (var i = 0; i < player.length; i++) {
+  player[i].addEventListener('click', function (event) {
+    var move = event.target.getAttribute('data-move');
+    playerMove(move);
+  })
+};
 
 
-  // var player = document.querySelectorAll('.player-move');
+// Losowanie liczby z zakresu 1-3 i zapisywanie w zmiennej
+// var compMove = function () {
+//   var move = Math.floor(Math.random() * 3 + 1);
+//   if (move == 1) {
+//     return 'paper';
+//   }
+//   if (move == 2) {
+//     return 'stone';
+//   }
+//   if (move == 3) {
+//     return 'scissors';
+//   }
+// }
 
-  // function playerMove() {
-  //   for (var i = 0; i < player.length; i++) {
-  //     document.querySelector(this.getAttribute("data-move")).addEventListener('click', playerMove);
-  //   }
-  // }
+function compMove() {
+  var computerMove = Math.floor((Math.random() * 3) + 1);
+  return computerMove;
+};
 
-  // Losowanie liczby z zakresu 1-3 i zapisywanie w zmiennej
-  var compMove = function () {
-    var move = Math.floor(Math.random() * 3 + 1);
-    if (move == 1) {
-      return 'paper';
-    }
-    if (move == 2) {
-      return 'stone';
-    }
-    if (move == 3) {
-      return 'scissors';
-    }
-  }
-
-  // W oparciu o wylosowaną liczbę, skrypt decyduje czy wynikiem jest remis, wygrana gracza, czy wygrana komputera
-  computerMove = compMove();
-  if ((move == computerMove)) {
-    show('Draw!');
-  } else if ((move == 'stone' && computerMove == 'scissors') ||
-    (move == 'paper' && computerMove == 'stone') ||
-    (move == 'scissors' && computerMove == 'paper')) {
-    playerResult++;
-    game(playerResult, gamesMax);
-    show('You won!');
-  } else {
-    computerResult++;
-    game(computerResult, gamesMax);
-    show('You lost!');
-  }
+// W oparciu o wylosowaną liczbę, skrypt decyduje czy wynikiem jest remis, wygrana gracza, czy wygrana komputera
+computerMove = compMove();
+if ((move == computerMove)) {
+  show('Draw!');
+} else if ((move == 'stone' && computerMove == 'scissors') ||
+  (move == 'paper' && computerMove == 'stone') ||
+  (move == 'scissors' && computerMove == 'paper')) {
+  playerResult++;
+  game(playerResult, gamesMax);
+  show('You won!');
+} else {
+  computerResult++;
+  game(computerResult, gamesMax);
+  show('You lost!');
 }
 
 // funkcja do wyświetlania tekstu na stronie
@@ -82,52 +75,36 @@ function show(text) {
 
 // Modal
 
-(function () {
-  var showModal = function (event) {
-    event.preventDefault();
-
-    var overlay = event.currentTarget.getAttribute('data-modal');
-
-    for (var i = 0; i < document.querySelectorAll(".show-modal").length; i++) {
-      document.querySelectorAll(".modal")[i].classList.remove("show");
-    };
-    document.querySelector(overlay).classList.add('show');
-    document.querySelector(overlay + ' .modal').classList.add('show');
-  };
-
-  var modalLinks = document.querySelectorAll('.show-modal');
-  var overlays = document.querySelectorAll('.overlay');
-
-  for (var i = 0; i < modalLinks.length; i++) {
-    modalLinks[i].addEventListener('click', showModal);
-  }
-
-  var hideModal = function (event) {
-    event.preventDefault();
-
-    for (var i = 0; i < overlays.length; i++) {
-      overlays[i].classList.remove('show');
-    }
-  };
-
-  var closeButtons = document.querySelectorAll('.modal .js--close-modal');
-
-  for (var i = 0; i < closeButtons.length; i++) {
-    closeButtons[i].addEventListener('click', hideModal);
-  }
-
-  for (var i = 0; i < overlays.length; i++) {
-    overlays[i].addEventListener('click', hideModal);
-  }
-
-  var modals = document.querySelectorAll('.modal');
-
+var showModal = function () {
   for (var i = 0; i < modals.length; i++) {
-    modals[i].addEventListener('click', function (event) {
-      event.stopPropagation();
-    });
-  }
-})();
+    modals[i].classList.remove('show');
+  };
+  document.getAttribute('data-modal').classList.add('show');
+  document.querySelector('#modal-overlay').classList.add('show');
+};
+
+var hideModal = function (event) {
+  event.preventDefault();
+  document.querySelector('#modal-overlay').classList.remove('show');
+};
+
+var closeButtons = document.querySelectorAll('.modal .js--close-modal');
+
+for (var i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].addEventListener('click', hideModal);
+}
+
+for (var i = 0; i < overlays.length; i++) {
+  overlays[i].addEventListener('click', hideModal);
+}
+
+var modals = document.querySelectorAll('.modal');
+
+for (var i = 0; i < modals.length; i++) {
+  modals[i].addEventListener('click', function (event) {
+    event.stopPropagation();
+  });
+}
 
 //  Wygrana lub przegrana
 var game = function () {
@@ -139,16 +116,7 @@ var game = function () {
 }
 
 
-// funkcja po kliknięciu
-// paper.addEventListener('click', function () {
-//   playerMove('paper');
-// });
-// stone.addEventListener('click', function () {
-//   playerMove('stone');
-// });
-// scissors.addEventListener('click', function () {
-//   playerMove('scissors');
-// });
+// Nowa gra
 newGame.addEventListener('click', function () {
   gamesMax = window.prompt('How many rounds do you want to play?');
   computerResult = 0;
